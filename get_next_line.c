1b5f67d14115361c018223dd5cd7d5795ff58173
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-11-11 23:00:00 +0000

    get_next_line.c : ft_getline partially implemented

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,14 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 22:09:28 by passunca          #+#    #+#             */
-/*   Updated: 2023/11/11 22:23:13 by passunca         ###   ########.fr       */
+/*   Updated: 2023/11/11 22:56:45 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+void	ft_getline(int fd, t_list **strs, int *c_read);
+int		ft_isnewline(t_list *strs);
 
 char	*get_next_line(int fd)
 {
@@ -23,6 +26,38 @@ char	*get_next_line(int fd)
 		return (NULL);
 	c_read = 1;
 	line = NULL;
+	ft_getline(fd, &strs, &c_read);
+	if (!strs)
+		return (NULL);
 	
+	return (line);
+}
 
+void	ft_getline(int fd, t_list **strs, int *c_read)
+{
+	char	*buffer;
+
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return ;
+	while (!ft_isnewline(*strs) && *c_read != 0)
+	{
+		*c_read = (int)read(fd, buffer, BUFFER_SIZE);
+		if ((!*strs && *c_read == 0) || (*c_read == -1))
+		{
+			free(buffer);
+			return ;
+		}
+		buffer[*c_read] = '\0';
+	}
+
+
+	
+	return ;
+}
+
+int ft_isnewline(t_list *strs)
+{
+
+	return (0);
 }
