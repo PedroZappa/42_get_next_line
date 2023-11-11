d18d2d0b7bae7cec58af3e5dfeb623cfcd8071c3
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-11-11 22:24:38 +0000

    get_next_line.c : NULL checks implemented

--- get_next_line.c
+++ get_next_line.c
@@ -1,33 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/11 22:10:04 by passunca          #+#    #+#             */
-/*   Updated: 2023/11/11 22:23:47 by passunca         ###   ########.fr       */
+/*   Created: 2023/11/11 22:09:28 by passunca          #+#    #+#             */
+/*   Updated: 2023/11/11 22:23:13 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include "get_next_line.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 7
-# endif
-
-// Node buffer to store read strs
-typedef struct s_list
+char	*get_next_line(int fd)
 {
-	char			*str;
-	struct s_list	*next;
-}					t_list;
+	static t_list	*strs;
+	char			*line;
+	int				c_read;
 
-// Get next line functions
-char	*get_next_line(int fd);
+	strs = NULL;
+	if (fd < 0 || BUFFER_SIZE == 0 || read(fd, &line, 0) < 0)
+		return (NULL);
+	c_read = 1;
+	line = NULL;
+	
 
-#endif
+}
