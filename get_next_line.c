d6ea221ab61de824616e8b836bdf684169d9e1e9
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-11-11 23:45:21 +0000

    get_next_line.c : ft_storestr() implemented

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 22:09:28 by passunca          #+#    #+#             */
-/*   Updated: 2023/11/11 22:56:45 by passunca         ###   ########.fr       */
+/*   Updated: 2023/11/11 23:44:54 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	ft_getline(int fd, t_list **strs, int *c_read);
 int		ft_isnewline(t_list *strs);
+void	ft_storestr(t_list **strs, char *buffer, int c_read);
 
 char	*get_next_line(int fd)
 {
@@ -29,6 +30,7 @@ char	*get_next_line(int fd)
 	ft_getline(fd, &strs, &c_read);
 	if (!strs)
 		return (NULL);
+
 	
 	return (line);
 }
@@ -37,11 +39,11 @@ void	ft_getline(int fd, t_list **strs, int *c_read)
 {
 	char	*buffer;
 
-	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
-	if (!buffer)
-		return ;
 	while (!ft_isnewline(*strs) && *c_read != 0)
 	{
+		buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+		if (!buffer)
+			return ;
 		*c_read = (int)read(fd, buffer, BUFFER_SIZE);
 		if ((!*strs && *c_read == 0) || (*c_read == -1))
 		{
@@ -49,15 +51,37 @@ void	ft_getline(int fd, t_list **strs, int *c_read)
 			return ;
 		}
 		buffer[*c_read] = '\0';
+		ft_storestr(strs, buffer, *c_read);
+		free(buffer);	
 	}
-
-
-	
 	return ;
 }
 
-int ft_isnewline(t_list *strs)
+void ft_storestr(t_list **strs, char *buffer, int c_read)
 {
+	int		i;
+	t_list	*last_str;
+	t_list	*new_str;
 
-	return (0);
+	new_str = malloc(sizeof(t_list));
+	if (!new_str)
+		return ;
+	new_str->next = NULL;
+	new_str->str = malloc(sizeof(char) * (c_read + 1));
+	if (!new_str->str)
+		return ;
+	i = 0;
+	while (buffer[i] && (i < c_read))
+	{
+		new_str->str[i] = buffer[i];
+		++i;
+	}
+	new_str->str[i] = '\0';
+	if (!*strs)
+	{
+		*strs = new_str;
+		return ;
+	}
+	last_str = ft_getlastnode(*strs);
+	last_str->next = new_str;
 }
