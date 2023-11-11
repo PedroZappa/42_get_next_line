a3c875aff7460fd8c45f4a8e6156d24587870a60
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-11-11 22:20:15 +0000

    get_next_line.h : node struct & basic includes added

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 22:10:04 by passunca          #+#    #+#             */
+/*   Updated: 2023/11/11 22:19:22 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 7
+# endif
+
+typedef struct s_list
+{
+	char			*str;
+	struct s_list	*next;
+}	t_list;
+
+#endif
