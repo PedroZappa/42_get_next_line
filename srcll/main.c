f47e56e426cf0b3dae1f7fe6477b5db79c1384a9
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-11-15 09:19:54 +0000

    srcll : Moved get_next_line implementation into srcll/ for future reference including tests

---
+++ srcll/main.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 08:47:17 by passunca          #+#    #+#             */
+/*   Updated: 2023/11/15 09:18:38 by passunca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include "get_next_line.h"
+
+int main()
+{
+	int n = 1;
+	char *str = NULL;
+	int fd = open("../files/vulf.txt", O_RDONLY);
+	// int fd = open("../get_next_line.h", O_RDONLY);
+	// int fd = open("../files/mini-vulf.txt", O_RDONLY);
+	// int fd = open("../files/read_error.txt", O_RDONLY);
+
+	str = get_next_line(fd);
+	// printf("str : %s", str);
+	// int i = 0;
+	while (str)
+	{
+		printf("Line\t%d:\t%s", n, str);
+		free(str);
+		// if(i == 0)
+		// {
+		// 	close(fd);
+		// 	i++;
+		// }
+		str = get_next_line(fd);
+		// printf("str : %s", str);
+		// if(i == 1)
+		// {
+		// 	fd = open("files/mini-vulf.txt", O_RDONLY);
+		// 	str = get_next_line(fd);
+		// 	++i;
+		// }
+		++n;
+	}
+	free(str);
+	return (0);
+}
