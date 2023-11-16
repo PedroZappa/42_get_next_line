d63c9756e438ed6680190fc00e241eb7a3c2e541
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-11-16 18:53:33 +0000

    get_next_line.c : prints each line including \n

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 08:47:17 by passunca          #+#    #+#             */
-/*   Updated: 2023/11/16 12:44:15 by passunca         ###   ########.fr       */
+/*   Updated: 2023/11/16 18:49:50 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,16 @@ int main()
 {
 	int n = 1;
 	char *str = NULL;
-	// int fd = open("files/vulf.txt", O_RDONLY);
+	int fd = open("files/vulf.txt", O_RDONLY);
 	// int fd = open("get_next_line.h", O_RDONLY);
-	int fd = open("files/mini-vulf.txt", O_RDONLY);
+	// int fd = open("files/mini-vulf.txt", O_RDONLY);
 	// int fd = open("files/read_error.txt", O_RDONLY);
 
 	// str = get_next_line(fd);
 	// int i = 0;
 	while ((str = get_next_line(fd)) != NULL)
 	{
-		printf("Line\t%d:\t%s", n, str);
+		printf("%d:\t%s", n, str);
 		free(str);
 		// if(i == 0)
 		// {
