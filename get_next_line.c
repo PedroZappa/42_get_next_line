4587cc61bfcd800dbd92146206db9e24a6b4ac5d
Zedro <45104292+PedroZappa@users.noreply.github.com>
2023-11-20 20:00:44 +0000

    get_next_line.c : ft_getline re-implemented :recycle:

--- get_next_line.c
+++ get_next_line.c
@@ -6,45 +6,67 @@
 /*   By: passunca <passunca@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 09:23:19 by passunca          #+#    #+#             */
-/*   Updated: 2023/11/20 19:49:20 by passunca         ###   ########.fr       */
+/*   Updated: 2023/11/20 19:59:50 by passunca         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	ft_getline(int fd, char *substr);
-static void	ft_gettillnl(char *vault);
-static void	ft_getrest(char *vault);
+static char	*ft_getline(int fd, char *vault);
+static char	*ft_gettillnl(char *vault);
+static char	*ft_getrest(char *vault);
 
-char	*get_next_line(int fd)
+char		*get_next_line(int fd)
 {
 	static char	*vault;
 	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	vault = ft_getline(fd, &vault);
+	vault = ft_getline(fd, vault);
 	if (!vault)
 		return (NULL);
-	line = ft_gettillnl(&vault);
-	vault = ft_getrest(&vault);
+	line = ft_gettillnl(vault);
+	vault = ft_getrest(vault);
 	return (line);
 }
 
 /* Read BUFFER_SIZE bytes from 'fd' and store in 'vault'
  * */
-static int	ft_getline(int fd, char *substr)
+static char		*ft_getline(int fd, char *vault)
 {
+	char	*buffer;
+	int		bytes_read;
+
+	buffer = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	bytes_read = 1;
+	while (!ft_strchr(*vault, '\n'))
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		if (bytes_read <= 0)
+		{
+			free(buffer);
+			free(vault);
+			return (NULL);
+		}
+		buffer[bytes_read] = '\0';
+		vault = ft_strjoin(vault, buffer);
+	}
+	free(buffer);
+	return (vault);
 }
 
 /*	Get line terminate by '\n' from 'vault'
  * */
-static void	ft_gettillnl(char *vault)
+static char		*ft_gettillnl(char *vault)
 {
+
 }
 
 /*	Clear already printed chars in 'vault'
  * */
-static void	ft_getrest(char **vault)
+static char		*ft_getrest(char **vault)
 {
 }
