f3863cd68aeb61319b7edaebde5c00b1beb42edf
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-09 10:00:24 +0100

    main.c : generalized, takes 1 commandline arg

--- main.c
+++ main.c
@@ -10,24 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <fcntl.h>
 # include "lib/libft/libft/libft.h"			// libft library
 # include "lib/libft/ft_printf/ft_printf.h" 	// ft_printf
 #include "srcb/get_next_line.h"
 #include "srcll/get_next_line.h"
 
-int main()
+int main(int argc, char **argv)
 {
 	int n = 1;
 	char *str = NULL;
-	// int fd = open("srcb/get_next_line.h", O_RDONLY);
-	// int fd = open("files/mini-vulf.txt", O_RDONLY);
-	int fd = open("files/vulf.txt", O_RDONLY);
-	// int fd = open("files/read_error.txt", O_RDONLY);
+	int fd;
+
+	if (argc != 2)
+		return (0);
+	fd = open(argv[1], O_RDONLY);
 
 	ft_printf("Testing get_next_line\n\n");
-	
 	while ((str = get_next_line(fd)) != NULL)
 	{
 		ft_printf("%d:\t%s", n, str);
