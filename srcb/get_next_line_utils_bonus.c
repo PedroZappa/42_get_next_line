db7e3f42400581fe84aaf8264fc1e33ffe2390df
Zedro <45104292+PedroZappa@users.noreply.github.com>
2024-05-15 09:28:19 +0100

    gnl : fixed bonus header linking

--- srcb/get_next_line_utils_bonus.c
+++ srcb/get_next_line_utils_bonus.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 /// @brief		Join two strings
 /// @param s1	First string
